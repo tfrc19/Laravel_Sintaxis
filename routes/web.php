20f8f4cc960ef9bc4913888b2316292449d4c89b
Fernando Roque <telmo_ferc@hotmail.com>
2021-01-23 21:34:32 +0100

    Otros

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('welcome');
 });
+Route::get('/',function(){
+    return view('welcome');
+});
 
 Route::get('/blog','paginasController@create');
 Route::get('/contacto','paginasController@contacto');
